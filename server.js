b01fb97de27f6aff80c4f1b5e7ad092fa435dd29
alexandr <129582972+Lalchik584@users.noreply.github.com>
2026-05-12 22:58:43 +0300

    Update server.js 89348937898468

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const { v4: uuidv4 } = require('uuid');
 const fs = require('fs');
 const socketIo = require('socket.io');
 const qr = require('qr-image');
+const SessionLogger = require('./session-logger');
 
 const app = express();
 const server = http.createServer(app);
@@ -20,7 +21,6 @@ const PORT = process.env.PORT || 5000;
 const HOST = '0.0.0.0';
 const DB_FILE = path.join(__dirname, 'quizzes.json');
 const FEEDBACK_FILE = path.join(__dirname, 'feedbacks.json');
-const SessionLogger = require('./session-logger');
 
 // ========== БАЗЫ ДАННЫХ ==========
 let quizzes = [];
